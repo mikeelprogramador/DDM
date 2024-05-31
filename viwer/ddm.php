9d6dc007fa19d24ecddee984b783505a4175aa76
juadacaf <128726718+juadacaf@users.noreply.github.com>
2024-05-31 12:15:37 -0500

    Add files via upload

--- viwer/ddm.php
+++ viwer/ddm.php
@@ -21,7 +21,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
   </head>
-  <body>
+  <body class="body">
 
   <nav class="navbar navbar-expand-lg bg-gris-oscuro" id="nav">
   <div class="container-fluid">
