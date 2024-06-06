64f1c0fc48db61ea7683d567ed7f66ba07ba485a
juadacaf <128726718+juadacaf@users.noreply.github.com>
2024-06-06 07:50:45 -0500

    version juan 0.3

--- check-in.php
+++ check-in.php
@@ -14,23 +14,34 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Registro</title>
-    <link rel="stylesheet" href="css/stylo2.css">
     <link rel="stylesheet" href="css/stylo3.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
     
-    <form action="verificacion.php?log=0" method="post">
-        <input type="text" name="nom" placeholder="Ingrese tu nombre" required>
-        <input type="text" name="apellido" placeholder="apellido" required>
-        <input type="text" name="email" placeholder="correo" required>
-        <input type="password" name="clave" placeholder="contraeña" required>
-        <input type="submit" name="enviar">
-        <a href="login.php">Iniciar sesion</a>
-    </form>
-    
+  <div class="container">
+        <h2>Registro</h2>
+        <form action="verificacion.php?log=0" method="post" onsubmit="return validateForm()">
+            <input type="text" name="nom" placeholder="Ingrese tu nombre" required>
+            <input type="text" name="apellido" placeholder="Apellido" required>
+            <input type="text" name="email" placeholder="Correo" required>
+            <div class="clave">
+                <input type="password" id="clave" name="clave" placeholder="Contraseña" required>
+                <img id="toggle-password" class="toggle-password" src="img/ojo1.png" alt="Mostrar contraseña">
+            </div>
+            <div class="clave">
+                <input type="password" id="confirm_clave" name="confirm_clave" placeholder="Confirmar Contraseña" required>
+                <img id="toggle-confirm-password" class="toggle-password" src="img/ojo1.png" alt="Mostrar contraseña">
+            </div>
+            <div id="error" class="error"></div>
+            <input type="submit" name="enviar" value="Finalizar Registro">
+        </form>
+        <a href="login.php">Iniciar sesión</a>
+    </div>
+   
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="js/contra.js"></script>
 </body>
 </html>
