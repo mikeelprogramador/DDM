d08bc43d6b2e9792fff12760e5100ca724b73063
juadacaf <128726718+juadacaf@users.noreply.github.com>
2024-06-11 09:19:46 -0500

    Add files via upload

--- view/user/navbar-user.php
+++ view/user/navbar-user.php
@@ -25,7 +25,7 @@
 
   <nav class="navbar navbar-expand-lg bg-gris-oscuro" id="nav">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">LOGO</a>
+    <img src="../../img/logo.png" alt="" width="5%">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -73,7 +73,7 @@
       <form class="d-flex" role="search">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Carrito</a>
+        <a class="nav-link active" aria-current="page" href="#"><img src="../../img/carro.png" alt=""></a>
         </li> </ul>
         <input class="form-control me-2" type="search" placeholder="Buscar productos" aria-label="Search">
         <button class="btn btn-outline-success " type="submit">Buscar</button>
