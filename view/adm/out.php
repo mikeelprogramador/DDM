83c3c69c7b438d415bf0e2adb927fd4e615c1e2f
Mike <128723321+mikeelprogramador@users.noreply.github.com>
2024-06-20 09:47:08 -0500

    Delete view directory

--- view/adm/out.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if( ! isset($_SESSION)) session_start();
-    include_once("../../metodos/clas-login.php");
-    Login::actualizarEstadoUser(2, $_SESSION['id']);
-    session_destroy();
-    setcookie(session_name(), "", time() - 3600, "/");
-    header("location: ../../index.php");
